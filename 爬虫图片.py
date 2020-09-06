2a756c09c116e3b90f537a41d5eed570f4d882f4
xunjun.lu <xunjun.lu@unionpayadvisors.com>
2020-09-06 12:43:46 +0800

    初始化

---
+++ 爬虫图片.py
@@ -0,0 +1,21 @@
+from urllib import request
+import re
+
+from pip._vendor import chardet
+
+req = request.urlopen('https://coding.imooc.com/')
+html = req.read()
+encode_type = chardet.detect(html)
+# 进行相应解码，赋给原标识符（变量）
+html = html.decode(encode_type['encoding'])
+img_urls = re.findall(r'src=.+\.jpg', html)
+
+i = 0
+for url in img_urls:
+    url = str(url).replace("src=\"", "http:")
+    # wb + 以二进制方式进行打开储存、图片需要以二进制方式打开存储
+    f = open(str(i) + ".jpg", 'wb+')
+    req = request.urlopen(url)
+    img_file = req.read()
+    f.write(img_file)
+    i += 1
