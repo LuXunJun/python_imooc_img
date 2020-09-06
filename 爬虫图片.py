fa8125e12fc9d2f94ec1e36e87be7204a3cdca51
xunjun.lu <xunjun.lu@unionpayadvisors.com>
2020-09-06 20:09:48 +0800

    123

--- 爬虫图片.py
+++ 爬虫图片.py
@@ -6,9 +6,7 @@ from pip._vendor import chardet
 req = request.urlopen('https://coding.imooc.com/')
 html = req.read()
 encode_type = chardet.detect(html)
-# A同学
-# 进行相应解码，赋给原标识符（变量）
-# B同学
+# c 同学
 html = html.decode(encode_type['encoding'])
 img_urls = re.findall(r'src=.+\.jpg', html)
 
